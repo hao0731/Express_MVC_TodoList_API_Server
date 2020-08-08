d5edb603614687ff0d179870d4e5b52337676ac7
hao0731 <ss501801@gmail.com>
2020-08-08 22:42:37 +0800

    finish DAY04

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config({ path: path.resolve(__dirname, `./environments/${ process.env.NODE_ENV }.env`) });
+
+app.get('/', (req, res, next) => {
+    res.send('Hello, World!!');
+});
+
+app.listen(process.env.PORT, () => console.log(`http server is running at port ${ process.env.PORT }.`));
