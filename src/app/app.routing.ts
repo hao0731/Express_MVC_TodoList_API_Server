ded1c21135a2e9a372e6bb113d18ba34ce37bf0e
hao0731 <ss501801@gmail.com>
2020-08-10 12:46:01 +0800

    finish DAY06

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,4 +5,8 @@ router.get('/test', (req, res, next) => {
     res.send('test!');
 });
 
+router.post('/test', express.json(), (req, res, next) => {
+    res.send(JSON.stringify(req.body));
+});
+
 export default router;
